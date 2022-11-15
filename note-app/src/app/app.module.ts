64ba3a0ade625ae4a7584bde57639cdb615c08c5
Craig Wilson <cw66786@gmail.com>
2022-11-15 13:36:13 -0600

    components

--- note-app/src/app/app.module.ts
+++ note-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotesComponent } from './notes/notes.component';
+import { UserInterfaceComponent } from './user-interface/user-interface.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    UserInterfaceComponent
   ],
   imports: [
     BrowserModule,
